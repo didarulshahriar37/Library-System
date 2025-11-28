eaf07482d685f278eca4199ac7239ade9fdbd46a
Didarul Shahriar <didarul13579@gmail.com>
2025-11-28 21:40:09 +0600

    create person class

---
+++ Person.java
@@ -0,0 +1,11 @@
+abstract class Person{
+    private String name;
+    private String email;
+
+    Person(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    abstract void displayDetails();
+}
